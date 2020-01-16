c4aa1b6b4c893234700609346e3e49620c93bcd6
AymanSierra <ayman.a.makki@gmail.com>
2020-01-16 01:49:29 +0000

    Updating for End of Class

--- GitHub Test.R
+++ GitHub Test.R
@@ -1,2 +1,10 @@
-# This file is a test
+# This file is a test and was created on day 1 of class
 2 + 2
+
+# Good Practice Code For Everything
+set_here()
+as.integer(Sys.time()) %% 100000 -> time_seed
+set.seed(time_seed)
+time_seed
+sessionInfo()
+Sys.time()
